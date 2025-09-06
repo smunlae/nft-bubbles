47f400b84775fdd3e45549f8feab22f1016bf4fc
s30011 <smunlae@gmail.com>
2025-09-06 20:49:57 +0200

    Changed farcaster.json; Commented redirect

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01992007-eba3-df29-d2f5-c1108e2c4145',
-        permanent: false,
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async redirects() {
+//     return [
+//       {
+//         source: '/.well-known/farcaster.json',
+//         destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01992007-eba3-df29-d2f5-c1108e2c4145',
+//         permanent: false,
+//       },
+//     ];
+//   },
+// };
